1819793eb8a67066a86bed4d04303fafb221ef47
AlissaVelia <alyssavelia2980@gmail.com>
2020-04-21 10:50:45 +0700

    adding permission and adds

--- app/src/main/java/id/putraprima/retrofit/ui/MainActivity.java
+++ app/src/main/java/id/putraprima/retrofit/ui/MainActivity.java
@@ -5,10 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import id.putraprima.retrofit.R;
 import id.putraprima.retrofit.api.helper.ServiceGenerator;
@@ -27,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     public static final String SHARED_PREFS = "sharedPrefs";
     private TextView appName, appVersion;
     private EditText edtEmail, edtPassword;
+    private Button BtnLoad;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +45,91 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         edtEmail = findViewById(R.id.mainEdtEmail);
         edtPassword = findViewById(R.id.mainEdtPassword);
+        BtnLoad = findViewById(R.id.btnLoad);
         setupLayout();
         checkAppVersion();
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
 
+        // Reload AdListener
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+
+        });
+        //When user click again
+
+
+        // Add Click
+        BtnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                    // Reload Ads
+                    showInterstitial();
+                } else {
+                    Log.d("TAG", "The interstitial wasn't loaded yet.");
+                }
+            }
+        });
+        // Ad Events
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                Toast.makeText(MainActivity.this, "onAdLoaded()", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Toast.makeText(MainActivity.this,
+                        "onAdFailedToLoad() with error code: " + errorCode,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+//              permissions();
+                changePage();
+
+            }
+        });
+
+
+    }
+    //
+//     showing ads Intersitial
+    private void showInterstitial() {
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void setupLayout(){
@@ -109,6 +202,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleResep(View view) {
+        Intent intent = new Intent(this, ResepActivity.class);
+        startActivity(intent);
+    }
+
+    private void changePage() {
         Intent intent = new Intent(this, ResepActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/id/putraprima/retrofit/ui/SplashActivity.java
+++ app/src/main/java/id/putraprima/retrofit/ui/SplashActivity.java
@@ -3,15 +3,23 @@ package id.putraprima.retrofit.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import net.khirr.android.privacypolicy.PrivacyPolicyDialog;
 
 import id.putraprima.retrofit.R;
 import id.putraprima.retrofit.api.helper.ServiceGenerator;
@@ -23,52 +31,69 @@ import retrofit2.Response;
 
 public class SplashActivity extends AppCompatActivity {
     TextView lblAppName, lblAppTittle, lblAppVersion;
+    ConstraintLayout view;
     Context context;
+    private SharedPreferences preferences;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    private String app, version;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
-        context = getApplicationContext();
+
         setupLayout();
-        setAppInfo();
-        if (checkInternetConnection()) {
-            checkAppVersion();
+        if (checkInternetConnection() == true) {
+            privacyCheck();
         }
+        setAppInfo();
     }
 
     private void setupLayout() {
         lblAppName = findViewById(R.id.lblAppName);
         lblAppTittle = findViewById(R.id.lblAppTittle);
         lblAppVersion = findViewById(R.id.lblAppVersion);
+
         //Sembunyikan lblAppName dan lblAppVersion pada saat awal dibuka
-//        lblAppVersion.setVisibility(View.INVISIBLE);
-//        lblAppName.setVisibility(View.INVISIBLE);
+        //lblAppVersion.setVisibility(View.INVISIBLE);
+        //lblAppName.setVisibility(View.INVISIBLE);
     }
 
     private boolean checkInternetConnection() {
         //TODO : 1. Implementasikan proses pengecekan koneksi internet, berikan informasi ke user jika tidak terdapat koneksi internet
-        ConnectivityManager cm =
-                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null &&
-                activeNetwork.isConnectedOrConnecting();
-
-        return isConnected;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            return true;
+        }else{
+            //Toast.makeText(SplashActivity.this, "No internet connection", Toast.LENGTH_SHORT).show();
+            view = findViewById(R.id.splashConstrain);
+            Snackbar.make(view, "Failed to get connection to the server", Snackbar.LENGTH_LONG).show();
+            return false;
+        }
     }
 
 
     private void setAppInfo() {
         //TODO : 5. Implementasikan proses setting app info, app info pada fungsi ini diambil dari shared preferences
         //lblAppVersion dan lblAppName dimunculkan kembali dengan data dari shared preferences
-        //lblAppVersion.setVisibility(View.INVISIBLE);
-        //lblAppName.setVisibility(View.INVISIBLE);
-        SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(context);
+        //SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences preferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        app = preferences.getString("appName", "");
+        version = preferences.getString("appVersion", "");
+        lblAppName.setText(app);
         lblAppName.setVisibility(View.VISIBLE);
-        lblAppName.setText(preference.getString("appName","default"));
+        lblAppVersion.setText(version);
         lblAppVersion.setVisibility(View.VISIBLE);
-        lblAppVersion.setText(preference.getString("appVersion","default"));
+    }
 
+    private void mainActivity() {
+        Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 
     private void checkAppVersion() {
@@ -77,24 +102,68 @@ public class SplashActivity extends AppCompatActivity {
         call.enqueue(new Callback<AppVersion>() {
             @Override
             public void onResponse(Call<AppVersion> call, Response<AppVersion> response) {
-                Toast.makeText(SplashActivity.this, response.body().getApp(), Toast.LENGTH_SHORT).show();
+                AppVersion app = response.body();
+                Toast.makeText(SplashActivity.this, app.getApp(), Toast.LENGTH_SHORT).show();
+
                 //Todo : 2. Implementasikan Proses Simpan Data Yang didapat dari Server ke SharedPreferences
-                SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(context);
-                SharedPreferences.Editor editor = preference.edit();
-                editor.putString("appName",response.body().getApp());
-                editor.putString("appVersion",response.body().getVersion());
+                //SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+                SharedPreferences preferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("appName", app.getApp());
+                editor.putString("appVersion", app.getVersion());
                 editor.apply();
+
                 //Todo : 3. Implementasikan Proses Pindah Ke MainActivity Jika Proses getAppVersion() sukses
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
-                finish();
+                mainActivity();
             }
 
             @Override
             public void onFailure(Call<AppVersion> call, Throwable t) {
-                Toast.makeText(SplashActivity.this, "Gagal Koneksi Ke Server", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(SplashActivity.this, "Gagal Koneksi Ke Server", Toast.LENGTH_SHORT).show();
                 //Todo : 4. Implementasikan Cara Notifikasi Ke user jika terjadi kegagalan koneksi ke server silahkan googling cara yang lain selain menggunakan TOAST
+                Snackbar.make(view, "Failed to get connection to the server", Snackbar.LENGTH_LONG).show();
             }
         });
     }
+    private void privacyCheck() {
+        PrivacyPolicyDialog dialog = new PrivacyPolicyDialog(this,
+                "https://localhost/terms",
+                "https://localhost/privacy");
+
+        // Dialog
+        dialog.addPoliceLine("This application uses a unique user identifier for advertising purposes, it is shared with third-party companies.");
+        dialog.addPoliceLine("This application sends error reports, installation and send it to a server of the Fabric.io company to analyze and process it.");
+        dialog.addPoliceLine("This application requires internet access and must collect the following information: Installed applications and history of installed applications, ip address, unique installation id, token to send notifications, version of the application, time zone and information about the language of the device.");
+        dialog.addPoliceLine("All details about the use of data are available in our Privacy Policies, as well as all Terms of Service links below.");
+
+        // Title Dialog
+        dialog.setTitle("Terms Of Services");
+
+        // Terms
+        dialog.setTermsOfServiceSubtitle("If you click on (accept), you acknowledge that is makes the bla bla");
+        // Dialog set
+        dialog.setTitleTextColor(Color.parseColor("#222222"));
+        dialog.setAcceptButtonColor(ContextCompat.getColor(this, R.color.colorAccent));
+        dialog.show();
+
+        final Intent intent = new Intent(this, MainActivity.class);
+
+        dialog.setOnClickListener(new PrivacyPolicyDialog.OnClickListener() {
+            @Override
+            public void onAccept(boolean b) {
+                Log.e("SplashActivity", "Policies accepted");
+                startActivity(intent);
+                finish();
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.e("SplashActivity", "Policies not accepted");
+                finish();
+            }
+        });
+    }
+
+
 }
